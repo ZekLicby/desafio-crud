8aa03dfb6c5f4f3dabf2db8a07e40aa9932a84f5
Carlos Alberto Albuquerque Lins <carlos.lins@ferreiracosta.local>
2023-01-24 14:07:05 -0300

    Configurando o projeto

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+
+  return (
+
+      <h1>Teste de funcionalidade</h1>
+    
+  );
+}
+
+export default Home;
---
+++ src/theme/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+:root{
+    --white: #FFF;
+    --green: #5CB85C;
+    --yellow: #F0AD4E;
+    --red: #D9534F;
+    --blue: #337AB7;
+}
+`
